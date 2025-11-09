3ffbccdd6d083ba53d2a8f140e78f60728085c8f
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-11-09 17:27:45 -0300

    Aprendi a usar html no jsx

--- aulao_nexjs/fundamentos/src/pages/index.jsx
+++ aulao_nexjs/fundamentos/src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function Inicio(){
-  return "Inicio"
+  return(
+    <div>
+      <h1>Fundamentos de next.js e react</h1>
+      <h2>Vou estudar esse framework</h2>
+    </div>
+  )
 }
