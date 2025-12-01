24057096fcb593e87468cc8b226178869179c28e
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-12-01 19:17:44 -0300

    Aprendendo a referenciar componentes

---
+++ aulao_nexjs/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,14 @@
+import Cabecalho from "../../components/Cabecalho";
+
+export default function Exemplo(){
+  return (
+    <div>
+      <h1>Evidenciando outro componente nesse</h1>
+      <Cabecalho/>
+
+    </div> 
+    // Pode ser tag vazia ao inves da div
+    
+    
+  )
+}
