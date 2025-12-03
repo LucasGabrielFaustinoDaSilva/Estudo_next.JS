e7967bc7b3ee9cb8156938b09478409f67f26e44
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-12-03 18:45:10 -0300

    Estou aprendendo a como navegar pelas paginas

--- aulao_nexjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nexjs/fundamentos/src/pages/estiloso.jsx
@@ -1,10 +1,13 @@
+import Layout from '../../components/Layout'
 import styles from '../styles/Estiloso.module.css'
 import Link from 'next/link'
 export default function Estiloso(){
   return (
-    <div className={styles.roxo}>
-      <Link href="/">Voltar</Link>
+    <Layout titulo="Exemplo de CSS Modularizado">
+      <div className={styles.roxo}>
       <h1>Estilo usando CSS Módulo</h1>
     </div>
+    </Layout>
+    
   )
 }
