5a7fb886dad3081634a10e85ea1253d7cb35da90
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-12-11 19:35:48 -0300

    Aprendi a fazer integração com api e estou na aula de pre-rederização

--- aulao_nexjs/fundamentos/src/pages/api/hello.js
+++ aulao_nexjs/fundamentos/src/pages/api/hello.js
@@ -4,6 +4,6 @@ export default function handler(req, res) {
   res.status(200).json({ name: "Teste API",
   metodo: req.method,
   nome: req.query.nome,
-  idade: req.query.idade
+  idade: +req.query.idade
   });
 }
